5ad9d62eddd45ec4ba26912c5de15ee623291227
thepriefy <peachy-lovly-mom@hotmail.com>
2017-12-10 14:23:31 +0700

    day 3 (part 1), done!

--- src/day_04.js
+++ src/day_04.js
@@ -1,30 +1,35 @@
 var fs = require('fs');
 
-fs.readFile('../input/input_04.txt', 'utf8', function(err, data) {
+fs.readFile('../input/input_04.txt', 'utf8', function (err, data) {
     var input_text = data.toString().split("\n");
 
     /* part 1 */
     var valid_count = 0;
     var input_arr = {};
     var passphrase_arr = [];
+    var is_valid = true;
 
-    for(var i=0; i<Object.keys(input_text).length; i++)
-    {
+    for (var i = 0; i < Object.keys(input_text).length; i++) {
+        is_valid = true
         input_text[i] = input_text[i].replace("\r", "");
-        input_arr[i] = input_text[i].split(" ");
 
-        for (var j = 0; j < input_arr[i].length; j++)
-        {
-            if(passphrase_arr.indexOf(input_arr[i][j]) === -1)
-            {
+        input_arr[i] = input_text[i].split(" ");
+        passphrase_arr = [];
+
+        for (var j = 0; j < input_arr[i].length; j++) {
+            if (passphrase_arr.indexOf(input_arr[i][j]) === -1) {
                 passphrase_arr.push(input_arr[i][j]);
-                valid_count++;
             }
+            else {
+                is_valid = false;
+            }
+        }
+        if (is_valid) {
+            valid_count = valid_count + 1;
         }
     }
 
-    console.log(valid_count);
-    // console.log('Part 1 Ans: ' + sum);
+    console.log('Part 1 Ans: ' + valid_count);
 
     /* part 2 */
     // console.log('Part 2 Ans: ' + sum);
